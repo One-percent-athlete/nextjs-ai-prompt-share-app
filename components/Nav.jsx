c468a52794f2087a1b8db12cf579c7cc0b3dcfd5
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-10 20:59:59 +0900

    Update navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,9 @@ import { useState, userEffect } from "react"
 import { signIn, signOut, useSession, getProviders } from "next-auth/react"
 
 const Nav = () => {
+
+    const isUserLoggedIn = true
+
     return (
         <nav className="flex-between w-full mb-16 pt-3">
             <Link href="/" className="flex gap-2 flex-center">
@@ -17,7 +20,18 @@ const Nav = () => {
                     height={30}
                     className="object-contain"
                 />
+                <p className="logo_text">Promptopia</p>
             </Link>
+
+            <div className="sm:flex hidden">
+                {isUserLoggedIn ? (
+                    <div className="flex gap-3 md:gap-5">
+                        <Link href="/create-prompt" className="black_btn">Create Post</Link>
+                    </div>
+                ) : (
+                    <></>
+                )}
+            </div>
         </nav>
     )
 }
