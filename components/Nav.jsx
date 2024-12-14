38893f018702413a2b9e5707cd26cb6b785f6d04
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-14 23:01:33 +0900

    Complete navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -70,6 +70,7 @@ const Nav = () => {
                                         setToggleDropdown(false)
                                         signOut()
                                     }}
+                                    className="mt-5 w-full black_btn"
                                 >Sign Out
                                 </button>
                             </div>
@@ -78,7 +79,7 @@ const Nav = () => {
                 ) : (
                     <>
                         {providers && Object.values(providers).map((provider) => (
-                            <button type="button" key={provider.name} onClick={() => signIn(provider.id)} className="button_btn">
+                            <button type="button" key={provider.name} onClick={() => signIn(provider.id)} className="black_btn">
                                 Sign In
                             </button>
                         ))}
