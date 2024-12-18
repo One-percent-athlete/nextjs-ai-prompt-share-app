9cd28e37ac8f129264af219f0d11e39881cf4a71
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-18 20:58:11 +0900

    Update create prompt page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+'use client'
+
+import { Component, useState } from "react"
+import { useSession } from "next-auth/react"
+import { useRouters } from "@node_modules/next/navigation"
+
+import Form from "@components/form"
 
 const CreatePrompt = () => {
+    const [submitting, setSubmitting] = useState(false)
+    const [post, usePost] = useState({
+        prompt: "",
+        tag: ""
+    })
+
+    const createPrompt = async (e) => {
+
+    }
     return (
-        <div>CreatePrompt</div>
+        <Form
+            type="Create"
+            post={post}
+            setPost={post}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
     )
 }
 
