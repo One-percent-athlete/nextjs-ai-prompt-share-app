445f9240e5c4bbd79cd89ac7084c74d24960b194
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-19 23:32:35 +0900

    Update prompt card

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,7 +7,21 @@ import { useSession } from "@node_modules/next-auth/react"
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     return (
         <div className="prompt_card">
-            PromptCard
+            <div className="flex justify-between items-start gap-5">
+                <div className="felx-1 flex justify-start items-start gap-3 cursor-pointer">
+                    <Image src={post.creator.image}
+                        alt="user_image"
+                        width={40}
+                        height={40}
+                        className="rounded-full object-contain"
+                    />
+                    <div className="flex flex-col">
+                        <h3>{post.creator.username}</h3>
+                        <p>{post.creator.email}</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
