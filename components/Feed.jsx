7953c854327bc612ea9c3c019c94880405a9b6e2
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-19 23:12:40 +0900

    Update feed jsx

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PromptCard from "./PromptCard"
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className="mt-16 prompt_layout">
+            {data.map}
+        </div>
+    )
+}
 
 const Feed = () => {
+
+    const [searchText, setSearchText] = useState('')
+    const handleSearchChange = (e) => { }
+
     return (
         <section className="feed">
             <form className="relative w-full flex-center">
@@ -13,6 +26,11 @@ const Feed = () => {
                     className="search_input peer"
                 />
             </form>
+
+            <PromptCardList
+                data={[]}
+
+            />
         </section>
     )
 }
