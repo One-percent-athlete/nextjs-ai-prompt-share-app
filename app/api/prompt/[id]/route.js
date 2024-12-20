a7da1bd02688c69d6897f8ef152ee9c6238a72d0
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-20 22:21:23 +0900

    Create patch function in route js

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -5,9 +5,26 @@ export const GET = async (request, { params }) => {
     try {
         await connectToDB()
         const prompt = await Prompt.findById(params.id).populate('creator')
-        if (!prompt) return new Response('Prompt not found', { status: 400 })
+        if (!prompt) return new Response('Prompt not found', { status: 404 })
 
         return new Response(JSON.stringify(prompt), {status: 201})
+    } catch (error) {
+        return new Response("Failed to fetch prompt", {status: 500})
+    }
+}
+
+export const PATCH = async (request, { params }) => {
+    const { prompt, tag } = await request.json()
+
+    try {
+        await connectToDB()
+        const existingPrompt = await Prompt.findById(params.id).populate('creator')
+        if (!existingPrompt) return new Response('Prompt not found', { status: 404 })
+        existingPrompt.prompt = prompt
+        existingPrompt.tag = tag
+
+        await existingPrompt.save()
+        return new Response(JSON.stringify(existingPrompt), {status: 201})
     } catch (error) {
         return new Response("Failed to fetch prompt", {status: 500})
     }
