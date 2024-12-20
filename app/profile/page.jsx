24ef2e13dc85bf2d85cb72af1863e89d12c60bed
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-20 21:46:07 +0900

    Update route and page jsx

---
+++ app/profile/page.jsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { useState, useEffect } from 'react'
+import { useSession } from "@node_modules/next-auth/react"
+import { useRouter } from "@node_modules/next/navigation"
+
+import Profile from '@components/Profile'
+
+const MyProfile = () => {
+
+    const { data: session } = useSession()
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch(`/api/users/${session?.user.id}/posts`)
+            const data = await response.json()
+
+            setPosts(data)
+        }
+
+        if (session?.user.id) fetchPosts()
+    }, [])
+
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = async () => {
+
+    }
+
+    return (
+        <Profile
+            name="My"
+            desc="Welcome to your perosnalized page."
+            data={posts}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+        />
+    )
+}
+
+export default MyProfile
