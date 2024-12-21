69b9e3030dce2c3f86e6ce21fdb03a71951f5661
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-21 22:20:38 +0900

    Create delete function

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -24,7 +24,24 @@ export const PATCH = async (request, { params }) => {
         existingPrompt.tag = tag
 
         await existingPrompt.save()
-        return new Response(JSON.stringify(existingPrompt), {status: 201})
+        return new Response(JSON.stringify(existingPrompt), {status: 200})
+    } catch (error) {
+        return new Response("Failed to fetch prompt", {status: 500})
+    }
+}
+
+export const DELETE = async (request, { params }) => {
+    const { prompt, tag } = await request.json()
+
+    try {
+        await connectToDB()
+        const existingPrompt = await Prompt.findById(params.id).populate('creator')
+        if (!existingPrompt) return new Response('Prompt not found', { status: 404 })
+        existingPrompt.prompt = prompt
+        existingPrompt.tag = tag
+
+        await existingPrompt.save()
+        return new Response(JSON.stringify(existingPrompt), {status: 200})
     } catch (error) {
         return new Response("Failed to fetch prompt", {status: 500})
     }
